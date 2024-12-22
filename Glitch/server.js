0097a7f677768835d3fbf7442091b0ae41176305
Sebastian Parecattil <sdparecattil@gmail.com>
2024-12-22 00:09:27 -0600

    Updated comment on server

--- Glitch/server.js
+++ Glitch/server.js
@@ -2,16 +2,11 @@ const express = require("express");
 const app = express();
 const socket = require("socket.io");
 const path = require("path");
-let raspID;
-let queueCount = 0;
-let playerQueueID = {1: "NA", 2 : "NA"};
-let playerMaster = {};
-let usernameIDMaster = {};
-let stayPick = {};
-let confirmPick = {};
-let idInQueue = {};
-let pickCount = 0;
-let stayCount = 0;
+
+let raspID; // Raspberry Pi's socket id
+let queueCount = 0; // Current queue count - 1
+let playerMaster = {}; // Temporary socket ID info stored about the client
+let idInQueue = {}; // Clients that are currently in queue
 
 
 const bodyParser = require('body-parser');
@@ -54,85 +49,103 @@ io.on( "connection",function (socket)
   
   /////////////////////////////////////////////////////////////////////////////
   
+  // Login check to raspberry pi (web client -> server -> raspberry pi)
   socket.on ( 'login', function( data )
   {
     io.to(raspID).emit('login', data);
   });
   
+  // Register account send to raspberry pi (web client -> server -> raspberry pi)
   socket.on ( 'register', function( data )
   {
     io.to(raspID).emit('register', data);
   });
   
+  // Saving the raspberry pi's client ID (raspberry pi -> server)
   socket.on ( 'piID', function(data)
   {
     raspID = data.ID;
     console.log(raspID);
   });
   
+  // Successfull login (raspberry pi -> server -> web client)
   socket.on ( 'loginSuccessful', function(data)
   {
-    io.to(data.clientID).emit('loginSuccessful',data); //clientID and username
+    io.to(data.clientID).emit('loginSuccessful',data);
   });
   
+  // Incorrect username and password (raspberry pi -> server -> web client)
   socket.on ( 'incorrectUP', function(data)
   {
     io.to(data.clientID).emit('incorrectUP',{});
   });
   
+  // Account is not registered (raspberry pi -> server -> web client)
   socket.on ( 'registrationNeeded', function(data)
   {
     io.to(data.clientID).emit('registrationNeeded',{});
   });
   
+  // Successful registration (raspberry pi -> server -> web client)
   socket.on ( 'registerSuccessful', function(data)
   {
-    io.to(data.clientID).emit('registerSuccessful',data); //clientID and username
+    io.to(data.clientID).emit('registerSuccessful',data);
   });
   
+  // Username is taken (raspberry pi -> server -> web client)
   socket.on ( 'usernameTaken', function(data)
   {
     io.to(data.clientID).emit('usernameTaken',{});
   });
   
+  // Password is length is not satisfactory (raspberry pi -> server -> web client)
   socket.on ( 'passwordLength', function(data)
   {
     io.to(data.clientID).emit('passwordLength',{});
   });
   
+  // Characters used in the password are not satisfactory (raspberry pi -> server -> web client)
   socket.on ( 'passwordCharacters', function(data)
   {
     io.to(data.clientID).emit('passwordCharacters',{});
   });
   
+  // (raspberry pi -> server -> web client)
   socket.on ( 'passwordMatch', function(data)
   {
     console.log("Reached Password match");
     io.to(data.clientID).emit('passwordMatch',{});
   });
   
+  // (raspberry pi -> server -> web client)
   socket.on ( 'passwordSuccess', function(data)
   {
     io.to(data.clientID).emit('passwordSuccess',{});
   });
   /////////////////////////////////////////////////////////////////////////////
   
+  // Updating ranking for the database (web client -> server -> raspberry pi)
   socket.on ( 'updateRank', function(data)
   {
     io.except(raspID).emit('updateRank',data);
   });
   
+  // Updating personal ranking (raspberry pi -> server -> web client)
   socket.on ( 'updatePersonalRank', function(data)
   {
     io.to(data.clientID).emit('updatePersonalRank',data);
   });
   
+  // Updating leaderboard stats for the database (web client -> server -> raspberry pi)
   socket.on ( 'updateLeaderboardStats', function(data)
   {
     console.log(data);
     io.except(raspID).emit('updateLeaderboardStats',data);
   });
   
+  // Leaving the game (web client -> server -> raspberry pi & web client)
+  // Updating statistic for the database through the raspberry pi
+  // Notifying the other player in session X that their opponent has left
   socket.on ( 'leavingGame', function(data)
   {
     idInQueue[data.ID] = "NO";
@@ -148,6 +161,8 @@ io.on( "connection",function (socket)
   
   /////////////////////////////////////////////////////////////////////////////
   
+  // Placing web client in the correct room after socket id change based on the queue number assigned to them
+  // The socket id is updated in the database by sending to the raspberry pi
   socket.on ( 'checkQueueID', function(data)
   {
     if (data.ID == "NA" || data.ID == null) {
@@ -165,6 +180,9 @@ io.on( "connection",function (socket)
     console.log(data.username);
   });
   
+  // Assigning queue number to a web client 
+  // Odd player are placing in waiting
+  // Once an even player has queued, both player are sent to game screen
   socket.on ( 'checkQueue', function(data)
   {
     queueCount++;
@@ -192,22 +210,27 @@ io.on( "connection",function (socket)
     
   });
   
+  // Sending the opponents most and least used picks
   socket.on ( 'sendRoomData', function(data)
   {
     io.to(playerMaster[parseInt(data.IDOne)]).emit('roomData',{opponentMost : data.IDTwoMost, opponentLeast : data.IDTwoLeast});
     io.to(playerMaster[parseInt(data.IDTwo)]).emit('roomData',{opponentMost : data.IDOneMost, opponentLeast : data.IDOneLeast});
   });
   
+  // Letting the raspberry pi know when once a pick has been confirmed
+  // (web client -> server -> raspberry pi)
   socket.on ( 'pickConfirmed', function(data)
   {
     io.to(raspID).emit('pickConfirmed',data);
   });
   
+  // Sending opponents pick (raspberry pi -> server -> web client)
   socket.on ( 'opponentPick', function(data)
   {
     io.to(playerMaster[data.ID]).emit('opponentPick',data);
   });
   
+  // Notifying room that both user have made their picks
   socket.on ( 'bothConfirmed', function(data)
   {
     setTimeout(function(){
@@ -221,6 +244,7 @@ io.on( "connection",function (socket)
     io.to(raspID).emit('stayPressed',data);
   });
   
+  // Indicating the room that both players have chosen to stay
   socket.on ( 'bothStayed', function(data)
   {
     setTimeout(function(){
@@ -228,10 +252,12 @@ io.on( "connection",function (socket)
     },3000);
   });
   
+  // Updating password from the database via raspberry pi (web client -> server -> raspberry pi)
   socket.on( 'password', function ( data ){
     io.to(raspID).emit('updatePassword',data);
   });
   
+  // Deleting account from the database via raspberry pi (web client -> server -> raspberry pi)
   socket.on( 'deleteAccount', function ( data ){
     io.to(raspID).emit('deleteAccount',data);
   });
